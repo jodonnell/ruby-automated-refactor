db2539f4d2c5e09997cd925a28317903c2231c02
Jacob O'Donnell <jacobodonnell@gmail.com>
2014-11-05 20:47:54 -0500

    updated

--- ruby-automated-refactor.el
+++ ruby-automated-refactor.el
@@ -1,31 +1,80 @@
+(defvar extract-process-name "extract-process")
+
+(defvar extract-method-name nil)
+(defvar extract-method-current-buffer nil)
+(defvar extract-method-call-marker nil)
+
+(defvar extract-ruby-path
+  (let ((current (buffer-file-name)))
+    (expand-file-name (file-name-directory current)))
+  "Path to the backend Ruby code.")
+
 (defun extract-method(method-name)
   (interactive "sNew method name: ")
-  (save-excursion 
-    (replace-region-with-method method-name)
-    (find-spot-to-insert-new-method)
-    (insert-new-method method-name)
+  (setq extract-method-name method-name)
+  (setq extract-method-current-buffer (current-buffer))
+  (setup-process)
+  (move-code-to-new-method method-name)
+  (add-arguments-to-method method-name))
+
+(defun add-arguments-to-method(method-name) ; bad method name
+  (save-excursion
     (save-excursion
       (old-method-into-ripper))
     (new-method-into-ripper)
-    (let (args)
-      (setq args (grab-arguments))
-      (replace-string method-name (concat method-name " " args)))))
-    
+    (get-used)))
 
-(defun grab-arguments()
+(defun setup-process()
+  (when (not (and (extract-process) (process-live-p (extract-process))))
+    (progn
+      (start-extract-process)
+      (require-ruby-code))))
+
+(defun start-extract-process()
+  (let ((process-connection-type nil))  ; use a pipe
+    (start-process extract-process-name nil "irb")
+  (set-process-filter (extract-process) 'extract-process-filter)))
+;(delete-process (extract-process))
+
+(defun require-ruby-code()
+  (process-send-string (extract-process)
+                       (format (mapconcat #'identity
+                                          '("unless defined? ASTRefactor"
+                                            "$:.unshift '%s'"
+                                            "require 'extract'"
+                                            "require 'ripper'"
+                                            "ast_refactor = ASTRefactor.new"
+                                            "end\n")
+                                          "; ")
+                               extract-ruby-path)))
+
+(defun extract-process-filter(process output)
+  (set-buffer extract-method-current-buffer)
+  (if (string-match "\"\\(.*\\)\"$" output)
+      (progn
+      (let ((args (match-string 1 output)))
+        (set-buffer extract-method-current-buffer)
+        (beginning-of-buffer)
+        (search-forward (concat "def " extract-method-name))
+        (insert " ")
+        (insert args)
+        (goto-char extract-method-call-marker)
+        (insert " ")
+        (insert args)))))
+
+(defun extract-process()
+  (get-process extract-process-name))
+
+(defun move-code-to-new-method(method-name)
   (save-excursion
-    (search-backward "=> ")
-    (forward-char 4)
-    (let (start)
-      (setq start (point))
-      (search-forward "\"")
-      (backward-char)
-      (buffer-substring-no-properties start (point)))))
-
+    (replace-region-with-method method-name)
+    (find-spot-to-insert-new-method)
+    (insert-new-method method-name)))
 
 (defun replace-region-with-method(method-name)
   (kill-region (point) (mark))
   (insert-and-indent method-name)
+  (setq extract-method-call-marker (point-marker))
   (newline))
 
 (defun find-spot-to-insert-new-method()
@@ -41,55 +90,26 @@
   (newline))
 
 (defun old-method-into-ripper()
-  (beginning-of-defun 2)
-  (let (start old-method)
-    (setq start (point))
-    (end-of-defun)
-    (setq old-method (buffer-substring-no-properties start (point)))
-    (set-buffer "*ruby*")
-    (insert "require 'ripper'")
-    (comint-send-input)
-    (insert "a = Ripper.sexp('")
-    ; need to escape any '
-    (insert old-method)
-    (insert "')")
-    (comint-send-input))
-  (get-all-used))
+  (beginning-of-defun)
+  (process-send-string (extract-process) (ripper-sexp "a")))
 
 (defun new-method-into-ripper()
-  (beginning-of-defun)
-  (let (start new-method)
+  (end-of-defun)
+  (process-send-string (extract-process) (ripper-sexp "b")))
+
+(defun ripper-sexp(var)
+  (concat var " = Ripper.sexp(<<ruby_emacs_extract_string\n" (get-method) "\nruby_emacs_extract_string\n)\n"))
+
+(defun get-method() 
+  ; need to escape any '
+  (let (start)
     (setq start (point))
     (end-of-defun)
-    (setq new-method (buffer-substring-no-properties start (point)))
-    (set-buffer "*ruby*")
-    (insert "b = Ripper.sexp('")
-    ; need to escape any '
-    (insert new-method)
-    (insert "')")
-    (comint-send-input))
-  (get-used))
+    (buffer-substring-no-properties start (point))))
 
 (defun get-used()
-  (insert "def find_used_assigned_vars ruby_ast, var, assigned_vars;  next_is_var = false;  ruby_ast.each do |thing|;    return var.puh(thing) if next_is_var and assigned_vars.include? thing;    if thing.is_a? Array;      find_used_assigned_vars thing, var, assigned_vrs;    elsif thing == :@ident;      next_is_var = true;    end;  end;  return var;end; (find_used_assigned_vars b, [], results).join(' ')")
-  (comint-send-input))
-
-(defun get-all-used()
-  (insert "def get_assigned_vars ruby_ast, var, assignment=false;  next_is_var = false;  ruby_ast.each do |thing|;    return var.push(hing) if next_is_var;    if thing.is_a? Array;      get_assigned_vars thing, var, assignment;    elsif thing == :assign;      assignmet = true;    elsif thing == :@ident and assignment;      next_is_var = true;    end;  end;  return var;end;results = get_assigned_varsa, []")
-  (comint-send-input))
-
+  (process-send-string (extract-process) "((ast_refactor.get_variable_references b, []) & (ast_refactor.get_local_variables_from_caller a, [])).join(', ')\n"))
 
 (defun insert-and-indent(text)
   (insert text)
   (indent-according-to-mode))
-
-; find all local variables that are not defined
-; check current method for definition, including passed in parameters
-; if found pass in
-; look for usage of variables that are no longer defined (were defined in the extracted code)
-; inf-ruby
-; require 'ripper'
-; Ripper.sexp()
-
-; run vote tally, look for assigns get idents "sum"
-; look for var_field in boom that uses @ident sum if found pass it in
